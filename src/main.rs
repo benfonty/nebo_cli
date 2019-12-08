7ca1f3215969a4774951de1922f92b40dc7a9e62
Your Name <you@example.com>
2019-12-08 21:30:38 +0100

    first try with clap for arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{App, Arg};
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("nebo_cli")
+        .version("1.0")
+        .author("benoît F")
+        .about("rust version of nebo_cli")
+        .arg(Arg::with_name("env")
+                .long("env")
+                .value_name("env")
+                .help("the env")
+                .takes_value(true)
+                .required(true)
+                .possible_values(&["local", "cloudtest", "cloudtest2", "prod"])
+            )
+        .subcommand(
+            App::new("token")
+                .about("get the jwt token of a user")
+                    )
+        .get_matches();
+    
+        println!("{}", matches.subcommand_name().unwrap());
+        println!("{}", matches.value_of("env").unwrap())
 }
