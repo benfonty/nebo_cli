c9bf9c8ec57143b1227270ca224cb9a569a8a9cb
Your Name <you@example.com>
2019-12-12 23:00:12 +0100

    trying coding second step. lost in result of results

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,8 @@
 
 pub struct Env {
-    pub sso_url: &'static str
+    pub sso_url: &'static str,
+    pub client_id: &'static str,
+    pub sso_redirect_uri: &'static str
 }
 use std::collections::HashMap;
 
@@ -8,10 +10,14 @@ lazy_static! {
     pub static ref ENV: HashMap<&'static str, &'static Env> = {
         let mut m = HashMap::new();
         m.insert("local", &Env {
-            sso_url: "http://localhost:8080"
+            sso_url: "http://localhost:8080",
+            client_id: "toto",
+            sso_redirect_uri: "http://localhost:8080"
         });
         m.insert("prod",&Env  {
-            sso_url: "https://sso.myscript.com"
+            sso_url: "https://sso.myscript.com",
+            client_id: "toto",
+            sso_redirect_uri: "http://localhost:8080"
         });
         m
     };
--- src/token.rs
+++ src/token.rs
@@ -4,6 +4,7 @@ use reqwest::RedirectPolicy;
 use reqwest::Client;
 
 use std::error::Error;
+use dialoguer::PasswordInput;
 
 use super::common;
 
@@ -15,18 +16,43 @@ pub fn token(arg: &ArgMatches) -> Result<(), Box<dyn Error>> {
         .build()?;
     
     let env = arg.value_of("env").unwrap();
+    let login = arg.value_of("login").unwrap();
     let sso_url = common::ENV[env].sso_url;    
     
-    first_call(client, sso_url)
+    first_call(&client, env, sso_url)?;
+    let location = second_call(&client, sso_url, login)?;
+    Ok(())
 } 
 
-fn first_call(client: Client, sso_url: &str) -> Result<(), Box<dyn Error>> {
-    let first_response = client
-    .get(format!("{}/oauth/authorize",sso_url).as_str())
-    .send()?;
-    if first_response.status() != 302 {
+fn first_call(client: &Client, env: &str, sso_url: &str) -> Result<(), Box<dyn Error>> {
+    let response = client
+        .get(format!("{}/oauth/authorize",sso_url).as_str())
+        .query(&[
+            ("client_id", common::ENV[env].client_id),
+            ("response_type", "token"),
+            ("scope", "read"),
+            ("redirect_uri", common::ENV[env].sso_redirect_uri)
+        ])
+        .send()?;
+    if response.status() != 302 {
         return Err(Box::from("wrong answer from first call"))
     }
     Ok(())
+}
+
+fn second_call<'a>(client: &Client, sso_url: &str, login: &str)-> Result<(&'a str), Box<dyn Error>> {
+    let password = PasswordInput::new().with_prompt("password").interact()?;
+    let response = client
+        .post(format!("{}/public/customlogin",sso_url).as_str())
+        .form(&[("email", login), ("password", password.as_str())])
+        .send()?;
+    if response.status() != 302 {
+        return Err(Box::from("wrong answer from first call"))
+    }
+    response
+        .headers()
+        .get("location")
+        .ok_or ("aie")
+        .map(|s| s.to_str())
 }
 
