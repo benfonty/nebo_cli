8966cb4f09704f48d3c94728241991bbab436fb7
benfonty <benfonty@gmail.com>
2020-01-14 18:46:30 +0100

    refactor page modules

--- src/page/mod.rs
+++ src/page/mod.rs
@@ -103,3 +103,28 @@ fn call_share_api(env: &str, client: &Client, uuid: &str, signature: &str, title
     debug!("End Calling share api OK");
     Ok(())
 }
+
+pub fn delete_page(
+    env: &str, 
+    token: &str, 
+    uuid: &str, 
+    ) -> Result<(), Box<dyn Error>> {
+    info!("Begin deleting page {}", &uuid);
+    let response = common::get_default_client(token)
+        .delete(format!("{}{}/{}", common::ENV[env].neboapp_url, common::NEBO_API_URI_PAGES, uuid).as_str())
+        .send()?;
+        
+    let status = response.status();
+    let _dummy = response.text();
+    if status.is_success() {
+        info!("End deleting page {} OK", &uuid);
+    }
+    else if status == StatusCode::NOT_FOUND {
+        info!("End deleting page {} OK (was already deleted)", &uuid);
+    }
+    else {
+        return Err(Box::from("error during call to share api"));
+    }
+
+    Ok(())
+}
